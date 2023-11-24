3b9cda2ebd94176d207ec85d6a9c8fb6f7ad4812
dav1d992 <daviden@live.dk>
2023-11-24 21:23:13 +0100

    Adjusted member

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -1,18 +1,14 @@
+using API.Entities;
+
 namespace API.DTOs;
 public class MemberDto
 {
     public int Id { get; set; }
-    public string UserName { get; set; }
-    public string PhotoUrl { get; set; }
-    public int Age { get; set; }
-    public string KnownAs { get; set; }
+    public string ShortName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly DateOfBirth { get; set; }
     public DateTime Created { get; set; }
-    public DateTime LastActive { get; set; }
-    public string Gender { get; set; }
-    public string Introduction { get; set; }
-    public string LookingFor { get; set; }
-    public string Interests { get; set; }
-    public string City { get; set; }
-    public string Country { get; set; }
-    public List<PhotoDto> Photos { get; set; }
+    public string Workplace { get; set; }
+    public Role Role { get; set; }
 }
--- API/DTOs/MemberUpdateDto.cs
+++ API/DTOs/MemberUpdateDto.cs
@@ -1,9 +1,12 @@
+using API.Entities;
+
 namespace API.DTOs;
 public class MemberUpdateDto
 {
-    public string Introduction { get; set; }
-    public string LookingFor { get; set; }
-    public string Interests { get; set; }
-    public string City { get; set; }
-    public string Country { get; set; }
+    public string ShortName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public string Workplace { get; set; }
+    public Role Role { get; set; }
 }
